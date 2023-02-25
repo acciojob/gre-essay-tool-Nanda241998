678cec2f4279589504df1ea73aaa325c91ec96f9
Nanda241998 <117671873+Nanda241998@users.noreply.github.com>
2023-02-26 00:01:39 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,10 @@
 //your code here
+var evaluatedText = document.getElementById("evaluatedText");
+var wordCount = document.getElementById("wordCount");
+
+// Update the word count in real-time as the user types
+evaluatedText.addEventListener("input", function() {
+  var text = evaluatedText.value;
+  var count = text.trim().split(/\s+/).length;
+  wordCount.innerText = count;
+});
